9ac125e0c098f67c33d272c0fbd524f459f0b98e
Marc Felden <marc.felden@me.com>
2016-02-20 10:01:16 +0100

    Initial Commit

---
+++ TestKata2StringCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestKata2StringCalculator
+//
+//  Created by Marc Felden on 20.02.16.
+//  Copyright © 2016 Timm Kent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
